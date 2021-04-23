7d1b598542bcdc246d883ab1ebb9a0eaf5ff6732
Łukasz <26624@s.am.szczecin.pl>
2021-04-23 18:29:12 +0200

    Little Changes v2

--- Assets/Scripts/States/Movement.cs
+++ Assets/Scripts/States/Movement.cs
@@ -8,7 +8,6 @@ namespace LP
     public class Movement : StateData
     {
         public float Speed;
-        public float sSpeed;
         public override void UpdateAbiltiy(CharacterStateBase characterStateBase, Animator animator, AnimatorStateInfo stateInfo)
         {
 
@@ -44,37 +43,22 @@ namespace LP
             }
             if (VirtualInputManager.Instance.Right && VirtualInputManager.Instance.Up)
             {
-                p.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+                
                 p.transform.rotation = Quaternion.Euler(0f, 45f, 0f);
-
             }
             if (VirtualInputManager.Instance.Right && VirtualInputManager.Instance.Down)
             {
-                p.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
                 p.transform.rotation = Quaternion.Euler(0f, 135f, 0f);
-
             }
             if (VirtualInputManager.Instance.Left && VirtualInputManager.Instance.Up)
             {
-                p.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
                 p.transform.rotation = Quaternion.Euler(0f, 315f, 0f);
-
             }
             if (VirtualInputManager.Instance.Left && VirtualInputManager.Instance.Down)
             {
-                p.transform.Translate(Vector3.forward * Speed * Time.deltaTime);
                 p.transform.rotation = Quaternion.Euler(0f, 215f, 0f);
             }
-            if (VirtualInputManager.Instance.Sprint && animator.GetBool(Transition.Move.ToString()) == true)
-            {
-                animator.SetBool(Transition.Sprint.ToString(), true);
-                Speed = sSpeed;
-            }
-            else
-            {
-                animator.SetBool(Transition.Sprint.ToString(), false);
-                Speed = Speed;
-            }
+            
         }
     }
 }
