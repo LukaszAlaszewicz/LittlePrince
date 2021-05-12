609e310fc5df8c0ece965be332e1fcd823a4d3ae
JuliaDuda <farbusia1999@gmail.com>
2021-05-12 20:49:44 +0200

    Dodanie CutScene1 -Act 1
    Dodaje nową scene do Unity. Na scenie odtwarzany jest filmik dla Actu 1. Stworzyłam też NextSceneTimer scrypt specialnie stworzony do zmiany sceny. (emulator)

---
+++ Assets/Scripts/System/NextSceneTimer.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//By JuliaDuda
+//NextSceneTimer u¿ywany bedzie g³ównie dla cutscenek, po zakonczeniu scenki chemy aby gra przesz³a do nastepnej sceny
+public class NextSceneTimer : MonoBehaviour
+
+{
+    public int secondLeft = 49;
+    public bool takingAway = false;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        if(takingAway == false && secondLeft > 0)
+        {
+            StartCoroutine(TimerTake());
+        }
+
+        if(secondLeft == 0)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+    IEnumerator TimerTake()
+    {
+        takingAway = true;
+        yield return new WaitForSeconds(1);
+        secondLeft -= 1;
+        takingAway = false;
+    }
+}
