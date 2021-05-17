bf46856ad1a3121e92e45178f1a1e9d20f731154
Łukasz <26624@s.am.szczecin.pl>
2021-05-17 15:59:38 +0200

    Planeta Latarnika ep. 1

---
+++ Assets/Scripts/TestRUN/rebuild_sphereEditor.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RebuildPlane))]
+public class rebuild_sphereEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Reset Plane"))
+        {
+            RebuildPlane.ResetPlane(Selection.activeGameObject.GetComponent<RebuildPlane>().MF.sharedMesh);
+        }
+    }
+}
