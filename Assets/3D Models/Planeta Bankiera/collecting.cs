276ff479917b176af0b2cbeeddb663bb368a7b0b
szympix74 <56876853+szympix74@users.noreply.github.com>
2021-06-07 09:48:30 +0200

    bankier_update

---
+++ Assets/3D Models/Planeta Bankiera/collecting.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class collecting : MonoBehaviour
+{
+    public float speed;
+    //public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "Player")
+        {
+            col.GetComponent<coins>().coinz++;
+            Destroy(gameObject);
+        }
+    }
+}
