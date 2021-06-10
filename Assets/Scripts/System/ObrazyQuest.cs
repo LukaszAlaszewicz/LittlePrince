05ed72ff9f3b60cd2d96d33391a4e57399738ed9
JuliaDuda <farbusia1999@gmail.com>
2021-06-10 16:40:48 +0200

    Quest Pijaka

---
+++ Assets/Scripts/System/ObrazyQuest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//By Julia Duda
+public class ObrazyQuest : MonoBehaviour
+{
+    public static int theScore;
+    public AudioSource collectSound;
+    public GameObject done;
+   
+    private GameObject score;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        ObrazyScore.theScore += 1;
+        done.GetComponent<BoxCollider>().enabled = false;
+    }
+
+
+
+}
