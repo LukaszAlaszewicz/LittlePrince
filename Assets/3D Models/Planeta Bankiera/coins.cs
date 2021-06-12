9bf24e0834d4bc22e3710e34feb89abf6ca07ae4
szympix74 <56876853+szympix74@users.noreply.github.com>
2021-06-12 21:51:51 +0200

    bankier_planet_finished

--- Assets/3D Models/Planeta Bankiera/coins.cs
+++ Assets/3D Models/Planeta Bankiera/coins.cs
@@ -6,6 +6,9 @@ public class coins : MonoBehaviour
 {
     private GUIStyle guiStyle = new GUIStyle();
     public int coinz = 0;
+    public GameObject teleport;
+    public GameObject lustro;
+    public GameObject particle;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,7 @@ public class coins : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        ShowMirror();
     }
 
     private void OnGUI()
@@ -24,6 +27,16 @@ public class coins : MonoBehaviour
         guiStyle.fontSize = 30;
         //guiStyle.color = white;
         guiStyle.normal.textColor = Color.white;
-        GUI.Label(new Rect(30, 30, 200, 20), "Zebrane monety: " + coinz, guiStyle);
+        GUI.Label(new Rect(30, 30, 200, 20), "Zebrane monety: " + coinz + "/14", guiStyle);
+    }
+
+    private void ShowMirror()
+    {
+        if (coinz == 14) 
+        {
+            teleport.SetActive(true);
+            lustro.SetActive(true);
+            particle.SetActive(true);
+        }
     }
 }
